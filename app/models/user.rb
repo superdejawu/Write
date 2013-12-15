4eab1d9b7c492978eabf2197f03f7f78612ff800
George Wu <georgewudesign@gmail.com>
2013-12-14 21:17:40 -0500

    Added validations to users and vitamins, displays user names instead of emails in vitamins.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :vitamins
+
+  validates :name, presence: true
 end
--- app/models/vitamin.rb
+++ app/models/vitamin.rb
@@ -3,4 +3,6 @@ class Vitamin < ActiveRecord::Base
 
 	
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+	validates :description, presence: true
+	validates :image, presence: true
 end
