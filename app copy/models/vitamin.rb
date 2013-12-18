f9b2e2ca8f4a8e0037e059a5b801cd67d1df44c2
George Wu <georgewudesign@gmail.com>
2013-12-18 00:43:37 -0500

    reset all files to pre-precompile state so it doesn't break. also did lots of styling related to header and hero area.

--- app copy/models/vitamin.rb
+++
@@ -1,8 +0,0 @@
-class Vitamin < ActiveRecord::Base
-	belongs_to :user
-
-	
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-	validates :description, presence: true
-	validates :image, presence: true
-end
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,14 @@
 //= require turbolinks
 //= require masonry/jquery.masonry
 //= require_tree .
+
+
+$(document).ready(function(){
+	$(window).on('scroll', function() {
+	    if ($(this).scrollTop() > 50) {
+	        $(".navbar-nav").stop(true).fadeTo(60, 0);
+	    } else {
+	        $(".navbar-nav").stop(true).fadeTo(60, 1);
+	    }
+	});
+});
