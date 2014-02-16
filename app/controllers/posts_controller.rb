fdf48ec3e5a9c7f1c70c50ca61ba605319f174fd
George Wu <georgewudesign@gmail.com>
2014-02-16 17:44:45 -0500

    Formatted post show and edit. Zenpen UI for edit now.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -58,7 +58,7 @@ class PostsController < ApplicationController
   def destroy
     @post.destroy
     respond_to do |format|
-      format.html {redirect_to posts_url }
+      format.html {redirect_to seeds_url }
     end
   end
 
