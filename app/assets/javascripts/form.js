a54f9431ccad90897896fcf00acd0b9aa17667d0
superdejawu <desktopgeo@gmail.com>
2014-03-04 18:07:02 -0500

    Fixed post submit button. JS controls rails form submit.

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -3,28 +3,14 @@ $(document).ready(function(){
 	$(".postbutton").on("click", function(){
 		$(this).closest(".container").siblings("#hidden_content").slideDown();
 		$(this).closest(".container").siblings(".bottombar").fadeIn();
-		$(this).fadeOut();
+		$(this).hide();
+		$(this).siblings("#hidden_button").fadeIn();
+
 	});
 	
-	$(window).scroll(function(){
-	var st = $(this).scrollTop(),
-	    winH = $(this).height(),
-	    /* you can set this add, 
-	    depends on where you want the animation to start
-	    for example if the section height is 100 and you set add of 50,
-	    that means if 50% of the section is revealed 
-	    on the bottom of viewport animate text
-	    */
-	    add = 150;
+	
+  $('#hidden_button').on('click', function() { $('#myForm').submit(); });
 
-		$('section').each(function(){
-		    var pos = $(this).position().top;
 
-		    if(st + winH >= pos + add){
-		        $(this).stop().animate({opacity:1, marginTop:10},'fast');
-		    }else{
-		        $(this).stop().animate({opacity:0, marginTop:0},'fast');
-		    }
-		});
-	});
+
 });
